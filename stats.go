0404719745e5c9f292cfd1683aa256cc57bfa019
Michele Finotto <m@finotto.org>
2022-12-19 09:27:49 +0100

    feat: Add last day hourly totals

--- stats.go
+++ stats.go
@@ -1,6 +1,8 @@
 package hourly_stats
 
 import (
+	"bytes"
+	"encoding/gob"
 	"strings"
 	"sync"
 	"time"
@@ -9,7 +11,7 @@ import (
 const hourlyBucketFormat = "06010215"
 
 type Stats struct {
-	data map[string]*bucket
+	Data map[string]*bucket
 	lock *sync.Mutex
 }
 
@@ -25,7 +27,7 @@ type hourly struct {
 
 func NewStats() *Stats {
 	return &Stats{
-		data: map[string]*bucket{},
+		Data: map[string]*bucket{},
 		lock: &sync.Mutex{},
 	}
 }
@@ -77,12 +79,12 @@ func (s *Stats) Incr(ref string) {
 	}
 	hour := time.Now().Format(hourlyBucketFormat)
 	s.lock.Lock()
-	b, ok := s.data[buck]
+	b, ok := s.Data[buck]
 	if !ok {
 		b = newBucket()
 	}
 	b.Incr(sub, hour)
-	s.data[buck] = b
+	s.Data[buck] = b
 	s.lock.Unlock()
 }
 
@@ -90,13 +92,14 @@ type Report struct {
 	Total         int                `json:"total"`
 	Average       float64            `json:"average"`
 	HourlyAverage map[int]float64    `json:"hourly_average,omitempty"`
+	LastDay       []int              `json:"last_day,omitempty"`
 	Subs          map[string]*Report `json:"subs,omitempty"`
 	keys          map[string]bool
 }
 
 func (s *Stats) Stats() *Report {
 	subs := map[string]*Report{}
-	for k, v := range s.data {
+	for k, v := range s.Data {
 		subs[k] = v.stats()
 	}
 	return &Report{
@@ -108,6 +111,7 @@ func (b *bucket) stats() *Report {
 	total := 0
 	hkeys := map[string]bool{}
 	subs := map[string]*Report{}
+	lastDay := make([]int, 24)
 	for k, v := range b.data {
 		r := v.stats()
 		total += r.Total
@@ -115,24 +119,35 @@ func (b *bucket) stats() *Report {
 			hkeys[h] = true
 		}
 		subs[k] = r
+		for i, d := range r.LastDay {
+			lastDay[i] += d
+		}
 	}
 	return &Report{
 		Total:   total,
 		Average: float64(total) / float64(len(hkeys)),
 		Subs:    subs,
+		LastDay: lastDay,
 	}
 }
 
 func (h *hourly) stats() *Report {
+	now24 := time.Now().Add(-24 * time.Hour)
+	currentH := time.Now().Hour()
 	total := 0
 	hours := map[int]int{}
 	hoursN := map[int]int{}
 	keys := map[string]bool{}
+	lastDay := make([]int, 24)
 	for k, v := range h.data {
 		total += v
 		t, err := time.Parse(hourlyBucketFormat, k)
 		if err != nil {
 			continue
+		}
+		if t.After(now24) {
+			lastInd := (t.Hour() - currentH) % 24
+			lastDay[lastInd] = v
 		}
 		hours[t.Hour()] += v
 		hoursN[t.Hour()] += 1
@@ -146,6 +161,7 @@ func (h *hourly) stats() *Report {
 		Total:         total,
 		Average:       float64(total) / float64(len(h.data)),
 		HourlyAverage: havg,
+		LastDay:       lastDay,
 		keys:          keys,
 	}
 }
